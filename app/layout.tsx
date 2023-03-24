3c473b845e9d05c9838644f0fb25dd931a0657b6
Prakhar <ptofficial29@gmail.com>
2023-03-24 19:51:25 +0530

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
   const footer = (
     <footer>
       <div className="border-t border-slate-400 mt-12 py-6 text-center text-slate-400">
-        <h3>Designed by PrakharT</h3>
+        <h3>Designed & Developed by PrakharT</h3>
       </div>
     </footer>
   );
