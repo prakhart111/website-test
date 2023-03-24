8a769aeea847646da0912d201f9cfc244508d85a
Prakhar <ptofficial29@gmail.com>
2023-03-24 18:22:28 +0530

    sample website

---
+++ app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <title>The Designer Dev</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
---
+++ app/layout.tsx
@@ -0,0 +1,49 @@
+import Image from "next/image";
+import Link from "next/link";
+import "../styles/globals.css";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const header = (
+    <header>
+      <div className="text-center bg-slate-800 p-8 my-6 rounded-md">
+        <Image
+          src="/logo.png"
+          width={40}
+          height={40}
+          className="mx-auto"
+          alt={"logo"}
+        />
+        <Link href="/">
+          <h1 className="text-2xl text-white font-bold mt-4">The Designer Dev</h1>
+        </Link>
+        <p className="text-slate-300">Welcome to our website.</p>
+        <p className="text-slate-300"> We're building it asap and it'll be live soon💻</p>
+      </div>
+    </header>
+  );
+
+  const footer = (
+    <footer>
+      <div className="border-t border-slate-400 mt-12 py-6 text-center text-slate-400">
+        <h3>Designed by PrakharT</h3>
+      </div>
+    </footer>
+  );
+
+  return (
+    <html>
+      <head />
+      <body>
+        <div className="mx-auto  max-w-2xl px-6">
+          {header}
+          {children}
+          {footer}
+        </div>
+      </body>
+    </html>
+  );
+}
